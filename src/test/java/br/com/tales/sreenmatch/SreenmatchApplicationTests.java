a1a1084c7699e162e3b0ce3c35dc6f53235a3f4d
Tales <taleskodama@gmail.com>
2025-09-26 17:31:43 -0300

    First commit

---
+++ src/test/java/br/com/tales/sreenmatch/SreenmatchApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tales.sreenmatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SreenmatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
