3258e9f6eea434734bf51e7af62a3a18adecc22a
Tales <taleskodama@gmail.com>
2025-10-03 19:57:19 -0300

    feat: Implement main interaction menu

--- src/main/java/br/com/tales/sreenmatch/SreenmatchApplication.java
+++ src/main/java/br/com/tales/sreenmatch/SreenmatchApplication.java
@@ -1,25 +1,18 @@
 package br.com.tales.sreenmatch;
 
-import br.com.tales.sreenmatch.model.DadosSerie;
-import br.com.tales.sreenmatch.service.ConsumoApi;
-import br.com.tales.sreenmatch.service.ConverteDados;
+import br.com.tales.sreenmatch.principal.Principal;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 
 @SpringBootApplication
 public class SreenmatchApplication implements CommandLineRunner {
   @Override
   public void run(String... args) throws Exception {
-    var consumoApi = new ConsumoApi();
-    var json =
-        consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=dd79755f");
-    System.out.println(json);
-    //        json = consumoApi.obterDados("https://coffee.alexflipnote.dev/random.json");
-    //        System.out.println(json);
-      ConverteDados conversor = new ConverteDados();
-      DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
-    System.out.println(dados);
+      Principal principal = new Principal();
+      principal.exibeMenu();
+
   }
 
   public static void main(String[] args) {
