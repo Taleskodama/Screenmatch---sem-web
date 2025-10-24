2db5afc1fb5f215512ec73aa16a5f3bdebceb878
Tales <taleskodama@gmail.com>
2025-10-23 23:39:35 -0300

    feat: Search by multiple criteria

--- src/main/java/br/com/tales/screenmatch/principal/Principal.java
+++ src/main/java/br/com/tales/screenmatch/principal/Principal.java
@@ -38,6 +38,7 @@ public class Principal {
                 2 - Buscar episódios
                 3 - Listar séries buscadas
                 4 - Buscar série por Título
+                5 - Buscar séries por Ator
 
                 0 - Sair
                 """;
@@ -59,6 +60,9 @@ public class Principal {
         case 4:
           buscarSeriePorTitulo();
           break;
+          case 5:
+              buscarSeriesPorAtor();
+              break;
         case 0:
           System.out.println("Saindo...");
           break;
@@ -135,5 +139,15 @@ public class Principal {
       }else{
       System.out.println("Série não encontrada!");
       }
+  }
+
+  private void buscarSeriesPorAtor() {
+    System.out.println("Qual o nome para a busca?");
+    var nomeAtor = leitura.nextLine();
+    System.out.println("Avaliações a partir de qual valor?");
+    var avaliacao = leitura.nextDouble();
+      List<Serie> seriesEncontradas = repositorio.findByAtoresContainingIgnoreCaseAndAvaliacaoGreaterThanEqual(nomeAtor,avaliacao);
+    System.out.println("Séries em que " + nomeAtor + " trabalhou: ");
+    seriesEncontradas.forEach(s -> System.out.println(s.getTitulo() + " avaliação: " + s.getAvaliacao()));
   }
 }
