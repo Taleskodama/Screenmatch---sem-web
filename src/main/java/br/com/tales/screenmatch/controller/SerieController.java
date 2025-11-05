09a7304f969dd0c72134619a3ff03b239adc0ece
Tales <taleskodama@gmail.com>
2025-11-04 21:18:44 -0300

    refactor: Move series business logic to SerieService

--- src/main/java/br/com/tales/screenmatch/controller/SerieController.java
+++ src/main/java/br/com/tales/screenmatch/controller/SerieController.java
@@ -1,35 +1,22 @@
 package br.com.tales.screenmatch.controller;
 
 import br.com.tales.screenmatch.dto.SerieDTO;
-import br.com.tales.screenmatch.model.Serie;
-import br.com.tales.screenmatch.repository.SerieRepository;
+import br.com.tales.screenmatch.service.SerieService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.stream.Collectors;
+
 
 @RestController
 public class SerieController {
 
-    @Autowired
-    private SerieRepository repositorio;
+@Autowired
+private SerieService servico;
 
     @GetMapping("/series")
     public List<SerieDTO> obterSeries(){
-    return repositorio.findAll().stream()
-        .map(
-            s ->
-                new SerieDTO(
-                    s.getId(),
-                    s.getTitulo(),
-                    s.getTotalTemporadas(),
-                    s.getAvaliacao(),
-                    s.getGenero(),
-                    s.getAtores(),
-                    s.getPoster(),
-                    s.getSinopse()))
-        .collect(Collectors.toList());
+    return servico.obterTodasAsSeries();
     }
 }
